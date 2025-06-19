9e97d81b1659ff2539d4a64fb079e71cfa22e308
Edgar Elio Quiñones Quiroga <edgarq354@gmail.com>
2025-06-19 01:48:41 -0400

    existe nombre

--- src/models/user.js
+++ src/models/user.js
@@ -14,6 +14,10 @@ export const User = sequelize.define('users',{
     username:{
         type: DataTypes.STRING,
         allowNull:false,
+        unique:{
+            args:true,
+            msg:'Username alreaddy exists',
+        },
         validate:{
             notNull:{
                 msg:'UserName is required'
